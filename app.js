35edac6fa090d96f76251bb7f9f68cfb6b8f5ae5
faithngetich <faithngetich188@gmail.com>
2018-07-10 21:27:41 +0300

    Delete book from list

--- app.js
+++ app.js
@@ -42,6 +42,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+// Delete book
+UI.prototype.deleteBook = function(target){
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear fields
 UI.prototype.clearFields = function(){
   document.getElementById('title').value = '';
@@ -49,7 +56,7 @@ UI.prototype.clearFields = function(){
   document.getElementById('isbn').value = '';
 }
 
-// Event listeners
+// Event listener for add book
 document.getElementById('book-form').addEventListener('submit',
 function(e){
   // Get form values
@@ -72,11 +79,26 @@ function(e){
     ui.addBookToList(book);
 
     // Show success
-    ui.showAlert('Book Addede!', 'success');
+    ui.showAlert('Book Added!', 'success');
 
     // Clear fiels
     ui.clearFields();
   }
   
+  e.preventDefault();
+})
+
+// Event listener for delete
+document.getElementById('book-list').addEventListener('click',
+function(e){
+
+  // Instantiate UI
+  const ui = new UI();
+
+  ui.deleteBook(e.target);
+
+  // Show message
+  ui.showAlert('Book Removed!', 'success');
+
   e.preventDefault();
 })
